9a10c18317647f415876d3a2f94ac7819dbdca46
matt-izatt <matt.izatt@hotmail.co.uk>
2021-05-02 14:20:04 +0100

    chore: code clean up

--- index.ts
+++ index.ts
@@ -2,20 +2,15 @@ import * as core from "@actions/core";
 import {getOctokit, context} from "@actions/github"
 
 try {
-    const message = core.getInput('message');
-    const github_token = core.getInput('GITHUB_TOKEN');
-
     if (context.payload.pull_request == null) {
         core.setFailed('No pull request found.');
     }
 
-    const pull_request_number = context.payload.pull_request.number;
-
-    const octokit = getOctokit(github_token);
+    const octokit = getOctokit(process.env.GITHUB_TOKEN);
     octokit.issues.createComment({
         ...context.repo,
-        issue_number: pull_request_number,
-        body: message
+        issue_number: context.payload.pull_request.number,
+        body: core.getInput('message')
     });
 
 } catch (error) {
