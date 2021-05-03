d73a130886443579705fbf03c54ba5570e998a18
matt-izatt <matt.izatt@hotmail.co.uk>
2021-05-04 00:36:59 +0100

    feat: send http request to bot on pr

--- index.js
+++ index.js
@@ -1,24 +1,135 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-    const message = core.getInput('message');
-    const github_token = core.getInput('GITHUB_TOKEN');
-
-    const context = github.context;
-    if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.');
-        return;
-    }
-    const pull_request_number = context.payload.pull_request.number;
-
-    const octokit = new github.getOctokit(github_token);
-    octokit.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request_number,
-        body: message
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-
-} catch (error) {
-    core.setFailed(error.message);
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var core = require("@actions/core");
+var github_1 = require("@actions/github");
+var axios_1 = require("axios");
+function run() {
+    return __awaiter(this, void 0, void 0, function () {
+        var instance;
+        return __generator(this, function (_a) {
+            try {
+                if (github_1.context.payload.pull_request == null) {
+                    core.setFailed('No pull request found.');
+                    return [2 /*return*/];
+                }
+                instance = axios_1["default"].create({
+                    baseURL: 'https://botz.learn-dev.uk',
+                    timeout: 1000,
+                    headers: { 'X-Custom-Header': 'foobar' }
+                });
+                instance.post('/pr', {
+                    "owner": "matt-izatt",
+                    "repo": "jest-test-repo",
+                    "issueNumber": 2,
+                    "body": "",
+                    "testResults": {
+                        "total": {
+                            "lines": {
+                                "total": 20,
+                                "covered": 20,
+                                "skipped": 0,
+                                "pct": 100
+                            },
+                            "statements": {
+                                "total": 21,
+                                "covered": 21,
+                                "skipped": 0,
+                                "pct": 100
+                            },
+                            "functions": {
+                                "total": 6,
+                                "covered": 6,
+                                "skipped": 0,
+                                "pct": 100
+                            },
+                            "branches": {
+                                "total": 0,
+                                "covered": 0,
+                                "skipped": 0,
+                                "pct": 100
+                            }
+                        },
+                        "/Users/matt/dev/projects/jest-test-repo/src/car.ts": {
+                            "lines": {
+                                "total": 20,
+                                "covered": 20,
+                                "skipped": 0,
+                                "pct": 100
+                            },
+                            "functions": {
+                                "total": 6,
+                                "covered": 6,
+                                "skipped": 0,
+                                "pct": 100
+                            },
+                            "statements": {
+                                "total": 21,
+                                "covered": 21,
+                                "skipped": 0,
+                                "pct": 100
+                            },
+                            "branches": {
+                                "total": 0,
+                                "covered": 0,
+                                "skipped": 0,
+                                "pct": 100
+                            }
+                        }
+                    }
+                }).then(function (response) {
+                    console.log(response);
+                })["catch"](function (error) {
+                    console.log(error);
+                });
+                // const octokit = getOctokit(process.env.GITHUB_TOKEN);
+                //
+                // octokit.issues.createComment({
+                //     ...context.repo,
+                //     issue_number: context.payload.pull_request.number,
+                //     body: core.getInput('message')
+                // });
+            }
+            catch (error) {
+                core.setFailed(error.message);
+            }
+            return [2 /*return*/];
+        });
+    });
 }
+run();
--- index.ts
+++ index.ts
@@ -1,18 +1,96 @@
 import * as core from "@actions/core";
-import {getOctokit, context} from "@actions/github"
+import {getOctokit, context} from "@actions/github";
+import axios from "axios";
 
-try {
-    if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.');
+async function run() {
+    try {
+        if (context.payload.pull_request == null) {
+            core.setFailed('No pull request found.');
+            return;
+        }
+
+        const instance = axios.create({
+            baseURL: 'https://botz.learn-dev.uk',
+            timeout: 1000,
+            headers: {'X-Custom-Header': 'foobar'}
+        });
+
+        instance.post('/pr', {
+            "owner": "matt-izatt",
+            "repo": "jest-test-repo",
+            "issueNumber": 2,
+            "body": "",
+            "testResults": {
+                "total": {
+                    "lines": {
+                        "total": 20,
+                        "covered": 20,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "statements": {
+                        "total": 21,
+                        "covered": 21,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "functions": {
+                        "total": 6,
+                        "covered": 6,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "branches": {
+                        "total": 0,
+                        "covered": 0,
+                        "skipped": 0,
+                        "pct": 100
+                    }
+                },
+                "/Users/matt/dev/projects/jest-test-repo/src/car.ts": {
+                    "lines": {
+                        "total": 20,
+                        "covered": 20,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "functions": {
+                        "total": 6,
+                        "covered": 6,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "statements": {
+                        "total": 21,
+                        "covered": 21,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "branches": {
+                        "total": 0,
+                        "covered": 0,
+                        "skipped": 0,
+                        "pct": 100
+                    }
+                }
+            }
+        }).then(function (response) {
+            console.log(response);
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        // const octokit = getOctokit(process.env.GITHUB_TOKEN);
+        //
+        // octokit.issues.createComment({
+        //     ...context.repo,
+        //     issue_number: context.payload.pull_request.number,
+        //     body: core.getInput('message')
+        // });
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    const octokit = getOctokit(process.env.GITHUB_TOKEN);
-    octokit.issues.createComment({
-        ...context.repo,
-        issue_number: context.payload.pull_request.number,
-        body: core.getInput('message')
-    });
-
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run();
