d1a467c036296b06a659681644a00c062244ab51
matt-izatt <matt.izatt@hotmail.co.uk>
2021-05-04 00:52:58 +0100

    feat: switch to /pr

--- index.ts
+++ index.ts
@@ -14,8 +14,67 @@ async function run() {
             timeout: 1000
         });
 
-        instance.get('/test')
-            .then(response => {
+        instance.post('/pr', {
+            "owner": "matt-izatt",
+            "repo": "jest-test-repo",
+            "issueNumber": 2,
+            "body": "",
+            "testResults": {
+                "total": {
+                    "lines": {
+                        "total": 20,
+                        "covered": 20,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "statements": {
+                        "total": 21,
+                        "covered": 21,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "functions": {
+                        "total": 6,
+                        "covered": 6,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "branches": {
+                        "total": 0,
+                        "covered": 0,
+                        "skipped": 0,
+                        "pct": 100
+                    }
+                },
+                "/Users/matt/dev/projects/jest-test-repo/src/car.ts": {
+                    "lines": {
+                        "total": 20,
+                        "covered": 20,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "functions": {
+                        "total": 6,
+                        "covered": 6,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "statements": {
+                        "total": 21,
+                        "covered": 21,
+                        "skipped": 0,
+                        "pct": 100
+                    },
+                    "branches": {
+                        "total": 0,
+                        "covered": 0,
+                        "skipped": 0,
+                        "pct": 100
+                    }
+                }
+            }
+        })
+            .then(function (response) {
                 console.log(response);
             })
             .catch(function (error) {
